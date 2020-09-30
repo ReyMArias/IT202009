e49f2e3c7e91d688effde3d844439c90a13953ab
rey4980 <57506826+rey4980@users.noreply.github.com>
2020-09-29 20:59:58 -0400

    fixing things

--- public_html/IT202009/loginreg/sample_reg.php
+++ public_html/IT202009/loginreg/sample_reg.php
@@ -40,7 +40,7 @@ if(isset($_POST["register"])){
     $db = getDB();
 	if(isset($db)){
 		//here we'll use placeholders to let PDO map and sanitize our data
-		$stmt = $db->prepare("INSERT INTO Users(email, password) VALUES(:email, :password)");
+		$stmt = $db->prepare("INSERT INTO `Users`(`email`, `password`) VALUES(:email, :password)");
 		//here's the data map for the parameter to data
 		$params = array(":email"=>$email, ":password"=>$hash);
 		$r = $stmt->execute($params);
