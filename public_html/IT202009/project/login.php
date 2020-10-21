1c5439f632894b391e321b41bcf8732b7c454d45
rey4980 <57506826+rey4980@users.noreply.github.com>
2020-10-21 18:58:00 -0400

    usernames sure are hard to put in

--- public_html/IT202009/project/login.php
+++ public_html/IT202009/project/login.php
@@ -1,6 +1,6 @@
 <?php require_once(__DIR__ . "/partials/nav.php"); ?>
     <form method="POST">
-        <label for="email">Email or Username:</label>
+        <label for="email">Email:</label>
         <input type="email" id="email" name="email" required/>
         <label for="p1">Password:</label>
         <input type="password" id="p1" name="password" required/>
@@ -11,8 +11,10 @@
 if (isset($_POST["login"])) {
     $email = null;
     $password = null;
+    $username = null;
     if (isset($_POST["email"])) {
         $email = $_POST["email"];
+        $username = $_POST["email"];
     }
     if (isset($_POST["password"])) {
         $password = $_POST["password"];
@@ -20,17 +22,13 @@ if (isset($_POST["login"])) {
     $isValid = true;
     if (!isset($email) || !isset($password)) {
         $isValid = false;
-        flash("Email or password missing");
+        flash("Email/Username or password missing");
     }
-    if (!strpos($email, "@")) {
-        if($email == isset($_POST["username"])){
-            $isValid = true;
-        }
-        else {
-            $isValid = false;
-            //echo "<br>Invalid email<br>";
-            flash("Invalid email");
-        }
+    if (!strpos($email, "@") && (":username" != $username)) {
+        if (":username" == $username)
+        $isValid = false;
+        //echo "<br>Invalid email<br>";
+        flash("Invalid User");
     }
     if ($isValid) {
         $db = getDB();
