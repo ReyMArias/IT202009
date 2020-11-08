f9493d113a09e897bb3fb372b9721ebda68e4e67
rey4980 <57506826+rey4980@users.noreply.github.com>
2020-11-08 17:57:42 -0500

    Testing the create

--- public_html/IT202009/project/test/test_create_scores.php
+++ public_html/IT202009/project/test/test_create_scores.php
@@ -8,37 +8,24 @@ if (!has_role("Admin")) {
 ?>
 
 <form method="POST">
-	<label>Name</label>
-	<input name="name" placeholder="Name"/>
-	<label>State</label>
-	<select name="state">
-		<option value="0">Incubating</option>
-		<option value="1">Hatching</option>
-		<option value="2">Hatched</option>
-		<option value="3">Expired</option>
-	</select>
-	<label>Base Rate</label>
-	<input type="number" min="1" name="base_rate"/>
-	<label>Mod Min</label>
-	<input type="number" min="1" name="mod_min"/>
-	<label>Mod Max</label>
-	<input type="number" min="1" name="mod_max"/>
+	<label>Score</label>
+	<input type="number" min="1" name="score"/>
+	<label>Time</label>
+	<input type="number" min="1" name="created"/>
 	<input type="submit" name="save" value="Create"/>
 </form>
 
 <?php
 if(isset($_POST["save"])){
 	//TODO add proper validation/checks
-	$name = $_POST["name"];
-	$score = $_Post["score"];
-	$screated = $_Post["created"];
+    $score = $_POST["score"];
+    $created = $_POST["created"];
 	$user = get_user_id();
 	$db = getDB();
-	$stmt = $db->prepare("INSERT INTO Scores (name, score, created, user_id) VALUES(:name, :score, :created, :user)");
+	$stmt = $db->prepare("INSERT INTO Scores (score, created, user_id) VALUES(:score, :created, :user)");
 	$r = $stmt->execute([
-		":name"=>$name,
 		":score"=>$score,
-		":created"=>$created
+		":created"=>$created,
 		":user"=>$user
 	]);
 	if($r){
