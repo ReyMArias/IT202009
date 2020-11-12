c2b0bf3685a7f66803585195ea542876e72bb100
rey4980 <57506826+rey4980@users.noreply.github.com>
2020-11-12 17:45:51 -0500

    more admin work. it's late

--- public_html/IT202009/project/test_view_pointshistory.php
+++ public_html/IT202009/project/test_view_pointshistory.php
@@ -17,7 +17,7 @@ if (isset($_GET["id"])) {
 $result = [];
 if (isset($id)) {
     $db = getDB();
-    $stmt = $db->prepare("SELECT PointsHistory.id,PointsHistory.reason, Users.username FROM PointsHistory JOIN Users on PointsHistory.user_id = Users.id where PointsHistory.id = :id");
+    $stmt = $db->prepare("SELECT PointsHistory.id,PointsHistory.reason, Users.username, Scores.score FROM PointsHistory JOIN Users on PointsHistory.user_id = Users.id LEFT JOIN Scores on PointsHistory.user_id = Score.user_id where PointsHistory.id = :id");
     $r = $stmt->execute([":id" => $id]);
     $result = $stmt->fetch(PDO::FETCH_ASSOC);
     if (!$result) {
@@ -32,7 +32,8 @@ if (isset($id)) {
         <div class="card-body">
             <div>
                 <p>Stats</p>
-                <div>Score: <?php safer_echo($result["reason"]); ?></div>
+                <div>Score: <?php safer_echo($result["score"]); ?></div>
+                <div>Reason of Change: <?php safer_echo($result["reason"]); ?></div>
                 <div>Owned by: <?php safer_echo($result["username"]); ?></div>
             </div>
         </div>
