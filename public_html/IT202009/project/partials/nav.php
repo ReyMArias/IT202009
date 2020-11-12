6731799ae43e128d846f4645ce33ae1dd1f33cc6
rey4980 <57506826+rey4980@users.noreply.github.com>
2020-11-11 20:42:53 -0500

    changint points, staring now

--- public_html/IT202009/project/partials/nav.php
+++ public_html/IT202009/project/partials/nav.php
@@ -13,6 +13,8 @@ require_once(__DIR__ . "/../lib/helpers.php");
         <?php if (has_role("Admin")): ?>
             <li><a href="test_create_scores.php">Create Score</a></li>
             <li><a href="test_list_scores.php">View Score</a></li>
+            <li><a href="test_create_pointshistory.php">Create Point History</a></li>
+            <li><a href="test_list_pointshistory.php">View Point History</a></li>
         <?php endif; ?>
         <?php if (is_logged_in()): ?>
             <li><a href="profile.php">Profile</a></li>
