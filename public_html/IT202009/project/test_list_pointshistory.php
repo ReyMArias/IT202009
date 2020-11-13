0d60b68ab2bdcd5df709913115ed12077d216184
rey4980 <57506826+rey4980@users.noreply.github.com>
2020-11-13 17:49:49 -0500

    swapping score for time

--- public_html/IT202009/project/test_list_pointshistory.php
+++ public_html/IT202009/project/test_list_pointshistory.php
@@ -14,7 +14,7 @@ if (isset($_POST["query"])) {
 }
 if (isset($_POST["search"]) && !empty($query)) {
     $db = getDB();
-    $stmt = $db->prepare("SELECT PointsHistory.id,points_change,reason, PointsHistory.user_id, Users.username, Scores.score as PlayerScore FROM PointsHistory JOIN Users on PointsHistory.user_id = Users.id JOIN Scores on PointsHistory.user_id = PlayerScores.user_id WHERE Users.username like :q LIMIT 10");
+    $stmt = $db->prepare("SELECT PointsHistory.id,points_change,reason, PointsHistory.user_id, PointsHistory.created, Users.username, Scores.score as PlayerScore FROM PointsHistory JOIN Users on PointsHistory.user_id = Users.id JOIN Scores on PointsHistory.user_id = PlayerScores.user_id WHERE Users.username like :q LIMIT 10");
     $r = $stmt->execute([":q" => "%$query%"]);
     if ($r) {
         $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
@@ -36,7 +36,10 @@ if (isset($_POST["search"]) && !empty($query)) {
                 <div>History Of:</div>
                         <div><?php safer_echo($r["username"]); ?></div>
                     </div>
-
+                    <div>
+                        <div>Time of Change:</div>
+                        <div><?php safer_echo($r["created"]); ?></div>
+                    </div>
                     <div>
                         <div>Score Change:</div>
                         <div><?php safer_echo($r["points_change"]); ?></div>
