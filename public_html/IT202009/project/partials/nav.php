fd55fa7f007e11316592b64265e4e8d276a4279a
rey4980 <57506826+rey4980@users.noreply.github.com>
2020-11-20 18:15:11 -0500

    a parenthesis 2

--- public_html/IT202009/project/partials/nav.php
+++ public_html/IT202009/project/partials/nav.php
@@ -12,7 +12,7 @@ require_once(__DIR__ . "/../lib/helpers.php");
         <?php endif; ?>
         <?php if (has_role("Admin")): ?>
             <li><a href="<?php echo getURL("test/test_create_scores.php"); ?>">Create Score</a></li>
-            <li><a href="<?php echo getURL("test/test_list_egg.php"); ?>">View Score</a></li>
+            <li><a href="<?php echo getURL("test/test_list_scores.php"); ?>">View Score</a></li>
             <li><a href="<?php echo getURL("test/test_create_pointshistory.php"); ?>">Create Point History</a></li>
             <li><a href="<?php echo getURL("test/test_list_pointshistory.php"); ?>">View Point History</a></li>
         <?php endif; ?>
