f16436012c5ddf9fc0ffbe81f003087625c6d047
rey4980 <57506826+rey4980@users.noreply.github.com>
2020-11-22 17:49:37 -0500

    Adding score table to home page

--- public_html/IT202009/project/home.php
+++ public_html/IT202009/project/home.php
@@ -7,4 +7,43 @@ if (isset($_SESSION["user"]) && isset($_SESSION["user"]["email"])) {
 }
 ?>
     <p>Welcome, <?php echo $email; ?></p>
+
+<?php
+$query = "";
+$results = [];
+if (isset($_POST["query"])) {
+    $query = $_POST["query"];
+}
+if (isset($_POST["search"]) && !empty($query)) {
+    $db = getDB();
+    $stmt = $db->prepare("SELECT Scores.id,score, user_id, Users.username FROM Scores JOIN Users on Scores.user_id = Users.id WHERE Users.username like :q LIMIT 10");
+    $r = $stmt->execute([":q" => "%$query%"]);
+    if ($r) {
+        $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+    else {
+        flash("There was a problem fetching the results");
+    }
+}
+?>
+<form method="POST">
+    <input name="query" placeholder="Search" value="<?php safer_echo($query); ?>"/>
+    <input type="submit" value="Search" name="search"/>
+</form>
+<div class="results">
+    <?php if (count($results) > 0): ?>
+        <div class="list-group">
+            <?php foreach ($results as $r): ?>
+                <div class="list-group-item">
+                    <div>
+                        <div>Score:</div>
+                        <div><?php safer_echo($r["score"]); ?></div>
+                    </div>
+                </div>
+            <?php endforeach; ?>
+        </div>
+    <?php else: ?>
+        <p>No results</p>
+    <?php endif; ?>
+</div>
 <?php require(__DIR__ . "/partials/flash.php");
