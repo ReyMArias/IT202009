10843bed5a2b1527ee1a4105a8343ad4da17684d
rey4980 <57506826+rey4980@users.noreply.github.com>
2020-11-25 17:18:33 -0500

    Scores on Home 8

--- public_html/IT202009/project/home.php
+++ public_html/IT202009/project/home.php
@@ -12,7 +12,7 @@ if (isset($_SESSION["user"]) && isset($_SESSION["user"]["email"])) {
 $db = getDB();
 $results = [];
 
-$stmt = $db->prepare("SELECT id, score, user_id FROM Scores WHERE Scores.user_id = :id LIMIT 10");
+$stmt = $db->prepare("SELECT id, score, user_id FROM Scores ORDER BY scores DESC WHERE Scores.user_id = :id LIMIT 10");
 $stmt->execute([":id" => get_user_id()]);
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
