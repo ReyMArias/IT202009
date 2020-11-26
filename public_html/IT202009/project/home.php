deb935d1257c94a8b3a45f1a419f4378f3a77204
rey4980 <57506826+rey4980@users.noreply.github.com>
2020-11-25 21:25:10 -0500

    Scoreboards 4

--- public_html/IT202009/project/home.php
+++ public_html/IT202009/project/home.php
@@ -7,4 +7,34 @@ if (isset($_SESSION["user"]) && isset($_SESSION["user"]["email"])) {
 }
 ?>
     <p>Welcome, <?php echo $email; ?></p>
+
+<?php
+$db = getDB();
+$results = [];
+$today = date("Y-m-d h:i:sa")
+
+$stmt = $db->prepare("SELECT id, score, user_id, created FROM Scores WHERE created BETWEEN $today and $today ORDER BY score DESC LIMIT 10");
+$stmt->execute([":id" => get_user_id()]);
+$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+?>
+
+<form method="POST">
+    <label>Last 10 Scores</label>
+</form>
+<div class="results">
+    <?php if (count($results) > 0): ?>
+        <div class="list-group">
+            <?php foreach ($results as $results): ?>
+                <div class="list-group-item">
+                    <div>
+                        <div>Score:</div>
+                        <div><?php safer_echo($results["score"]); ?></div>
+                    </div>
+                </div>
+            <?php endforeach; ?>
+        </div>
+    <?php else: ?>
+        <p>No results</p>
+    <?php endif; ?>
+</div>
 <?php require(__DIR__ . "/partials/flash.php");
