542862ebfea89abbf55b824f8e01d4379375d2c3
rey4980 <57506826+rey4980@users.noreply.github.com>
2020-11-28 19:16:36 -0500

    Today

--- public_html/IT202009/project/home.php
+++ public_html/IT202009/project/home.php
@@ -12,12 +12,13 @@ if (isset($_SESSION["user"]) && isset($_SESSION["user"]["email"])) {
 $db = getDB();
 $results = [];
 $weekly = [];
+$today = date('m/d/Y');
 
 $stmt = $db->prepare("SELECT score FROM Scores ORDER BY score DESC LIMIT 10");
 $stmt->execute();
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-$stmt = $db->prepare("SELECT score FROM Scores WHERE created BETWEEN '2020-11-22 23:05:55' AND '2020-11-22 23:06:02' ORDER BY score DESC LIMIT 10");
+$stmt = $db->prepare("SELECT score FROM Scores WHERE created BETWEEN '2020-11-22 23:05:55' AND $today ORDER BY score DESC LIMIT 10");
 $stmt->execute();
 $weekly = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
