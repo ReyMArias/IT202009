78fad1519aa550d627244a105500db885cad12a4
rey4980 <57506826+rey4980@users.noreply.github.com>
2020-12-09 17:58:11 -0500

    Creating Comp and bug fix

--- public_html/IT202009/project/lib/helpers.php
+++ public_html/IT202009/project/lib/helpers.php
@@ -76,8 +76,8 @@ function getURL($path){
 }
 
 function getPoints(){
-    if (is_logged_in() && isset($_SESSION["user"]["points"])) {
-        return $_SESSION["user"]["points"];
+    if (is_logged_in() && isset($_SESSION["points"])) {
+        return $_SESSION["points"];
     }
 }
 //end flash
