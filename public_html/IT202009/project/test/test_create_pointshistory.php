01c0d08a3967c1f7827dfbc4668400efd30a026b
rey4980 <57506826+rey4980@users.noreply.github.com>
2020-12-08 21:37:41 -0500

    Profile Points 7

--- public_html/IT202009/project/test/test_create_pointshistory.php
+++ public_html/IT202009/project/test/test_create_pointshistory.php
@@ -37,9 +37,9 @@ if(isset($_POST["save"])){
 		flash("Error creating: " . var_export($e, true));
 	}
 
-	$stmt = $db->prepare("UPDATE Users set points = (SELECT IFNULL(SUM(change), 0) FROM PointsHistory poiunts_change where points_change.user_id = :id) WHERE id = :id");
-	$p = $stmt->execute([":points" => "points", ":id" => get_user_id()]);
-	if ($p) {
+	$stmt = $db->prepare("UPDATE Users set points = (SELECT IFNULL(SUM(points_change), 0) FROM PointsHistory p where p.user_id = :id) WHERE id = :id");
+	$pch = $stmt->execute([":points" => "points", ":id" => get_user_id()]);
+	if ($pch) {
 		flash("Updated points");
 	}
 	else {
