612888f3f94ea470da732488f9f68923c9ff1223
rey4980 <57506826+rey4980@users.noreply.github.com>
2020-12-09 22:14:44 -0500

    Creating Comp 6 bug fix 3

--- public_html/IT202009/project/lib/helpers.php
+++ public_html/IT202009/project/lib/helpers.php
@@ -77,7 +77,7 @@ function getURL($path){
 
 function getPoints(){
     $db = getDB();
-    $stmt = $db->prepare("SELECT points FROM User where id=:id");
+    $stmt = $db->prepare("SELECT points FROM Users where id=:id");
     $stmt->execute([":id"=>get_user_id()]);
     flash(var_export($stmt->errorInfo(), true));
     $result = $stmt->fetch(PDO::FETCH_ASSOC);
