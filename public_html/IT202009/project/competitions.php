94b4936f559aebbe3862013f4ad9a18ab05cc0f5
rey4980 <57506826+rey4980@users.noreply.github.com>
2020-12-10 21:05:58 -0500

    Participation i guess

--- public_html/IT202009/project/competitions.php
+++ public_html/IT202009/project/competitions.php
@@ -50,7 +50,6 @@ else {
 }
 
 $stmt = $db->prepare("UPDATE Competitions set participants = (select count(1) from UserCompetitions where competition_id = :id) where id = :id");
-$compID = "competition_id"
 $a = $stmt->execute(":id" => $compID);
 if ($a) {
     flash("it worked", "success");
